77c641fee2646d53e815f182170b42e431ecafb5
jack-maber <jack-maber@live.co.uk>
2016-11-15 11:49:05 +0000

    Added boiler plate code from last weeks session

---
+++ BoilerPlate.py
@@ -0,0 +1,10 @@
+import wave
+import struct
+
+clip = wave.open('noise2.wav')
+frames = clip.getnframes()
+
+for i in range(0, frames):
+   soundinfo = clip.readframes(1)
+   clipstruct = struct.unpack("<h", soundinfo)
+   print(int(clipstruct[0]))
