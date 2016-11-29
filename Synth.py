447432c9e2e94b8dcff9007ecf773c57e38f5d3b
Kieran Goldsworthy <kieranalexstrasza@gmail.com>
2016-11-29 17:20:53 +0000

    Update Synth.py

--- Synth.py
+++ Synth.py
@@ -23,14 +23,25 @@ for i in range(0, Soundtuple[3]):
     for j in xrange(0,Soundtuple[0]):
         values.append(packaged_value)
 
-value_str = ''.join(values)
-def getSampleValueAt()
+
 
 def echo(sndFile, delay):
+    values = []
+    Channels = 1
     s1 = sndFile
     s2 = sndFile
-for index in range(delay, len(s1)):
-    echo = 0.6*
+    for index in range(delay, len(s1)):
+        echo = 0.6*s2[index-delay]
+        combo = (s1, index) + echo
+        s1[index] = combo
+        packaged_value = struct.pack("<h", s1[index])
+        for j in xrange(Channels):
+            values.append(packaged_value)
+    noise_out.writeframes(value_str)
+    noise_out.close()
+    return values
+
+
 
 noise_out.writeframes(value_str)
 
