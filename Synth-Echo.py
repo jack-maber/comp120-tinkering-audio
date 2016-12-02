677a2f148758bbf01afe1f851e9fc3101f187b29
jack-maber <jack-maber@live.co.uk>
2016-12-02 17:07:02 +0000

    Consolidated all functions into one file and created whitenoise generator

--- Synth-Echo.py
+++ Synth-Echo.py
@@ -12,6 +12,7 @@ BitDepth = 32767
 #Tuple of attributes of the wav file
 Soundtuple = (1, 2, samplerate, SampleLength, 'NONE', 'Not compressed')
 noise_out = wave.open('Alesis-Fusion-Acoustic-Bass-C2.wav','w')
+noise_in = wave.open('noise2.wav', 'r')
 noise_out.setparams(Soundtuple)
 
 values = []
@@ -19,10 +20,12 @@ values = []
 for i in range(0, Soundtuple[3]):
     value = math.sin(2.0 * math.pi * frequency * (i/Soundtuple[2])) * (amplitude * BitDepth)
     #print value
-    packaged_value = struct.pack('<h', value)
+    packaged_value = struct.unpack('<h', value)
 
     for j in xrange(0,Soundtuple[0]):
         values.append(packaged_value)
+
+print values
 
 
 #adds an echo to the wav file through a delay and lowering of volume
@@ -31,7 +34,7 @@ def echo(sndFile, delay):
     values = []
     Channels = 1
     s1 = sndFile
-    s2 = sndFile
+    s2 = sndFile[:]
     for index in range(delay, len(s1)):
         echo = 0.6*s2[index-delay]
         combo = (s1, index) + echo
@@ -42,11 +45,12 @@ def echo(sndFile, delay):
     value_str = ''.join(values)
     noise_out.writeframes(value_str)
     noise_out.close()
-    noise_out.writeframes(value_str)
     return values
-noise_out.close()
+
 
 #increases volume by making the amplitude of the waves bigger
 def increase_volume(frames, length):
     for i in xrange(length):
         frames[i] *= 2
+
+echo(noise_in, 8000)
--- ToneGenerator - Finished.py
+++ ToneGenerator - Finished.py
@@ -1,38 +1,118 @@
 import wave
 import struct
 import math
-
-noise_out = wave.open('noise2.wav', 'w')
-
-SAMPLE_RATE = float(44100) # Hz
-SAMPLE_LENGTH = 44100 * 4 # seconds
-FREQUENCY = 440 # Hz
-VOLUME = 1
-samples = FREQUENCY*SAMPLE_RATE
-BIT_DEPTH = 32767
-
-Soundtuple = (1, 4, 44100, (44100*4), 'NONE', 'Not compressed')
-noise_out.setparams(Soundtuple)
-
-values = []
-
-for i in range(0, SAMPLE_LENGTH):
-   value = math.sin(
-       2.0 *math.pi *
-       FREQUENCY *
-        ( i / SAMPLE_RATE)
-       )*                  \
-        (VOLUME * BIT_DEPTH)
-   packaged_value = struct.pack('<h', value)
-   for j in xrange(0, 1):
-        values.append(packaged_value)
-
-value_str = ''.join(values)
-noise_out.writeframes(value_str)
-
-noise_out.close()
+import random
 
 
-clip = wave.open('noise2.WAV')
-frames = clip.getnframes()
+def decoder():
+    import wave
+    import struct
 
+    clip = wave.open('noise2.wav', 'r')
+    frames = clip.getnframes()
+
+    for i in range(0, frames):
+        soundinfo = clip.readframes(1)
+        clipstruct = struct.unpack("<h", soundinfo)
+        print(int(clipstruct[0]))
+
+
+def tonegen():
+    noise_out = wave.open('noise2.wav', 'w')
+
+    SAMPLE_RATE = float(44100) # Hz
+    SAMPLE_LENGTH = 44100 * 4 # seconds
+    FREQUENCY = 440 # Hz
+    VOLUME = 1
+    BIT_DEPTH = 32767
+
+    Soundtuple = (1, 4, 44100, (44100*4), 'NONE', 'Not compressed')
+    noise_out.setparams(Soundtuple)
+
+    values = []
+
+    for i in range(0, SAMPLE_LENGTH):
+       value = math.sin(
+           2.0 *math.pi *
+           FREQUENCY *
+            ( i / SAMPLE_RATE)
+           )*                  \
+            (VOLUME * BIT_DEPTH)
+       packaged_value = struct.pack('<h', value)
+       for j in xrange(0, 1):
+            values.append(packaged_value)
+
+    value_str = ''.join(values)
+    noise_out.writeframes(value_str)
+
+    noise_out.close()
+
+
+def synth():
+    samplerate = float(44100)  # Hz
+    SampleLength = 44100 * 4  # seconds
+    frequency = 440  # Hz
+    amplitude = 1
+    BitDepth = 32767
+
+    # Tuple of attributes of the wav file
+    Soundtuple = (1, 2, samplerate, SampleLength, 'NONE', 'Not compressed')
+    noise_out = wave.open('Alesis-Fusion-Acoustic-Bass-C2.wav', 'w')
+    noise_out.setparams(Soundtuple)
+
+    values = []
+    # unpackaging the wav file so we get understandable values
+    for i in range(0, Soundtuple[3]):
+        value = math.sin(2.0 * math.pi * frequency * (i / Soundtuple[2])) * (amplitude * BitDepth)
+        # print value
+        packaged_value = struct.unpack('<h', value)
+
+        for j in xrange(0, Soundtuple[0]):
+            values.append(packaged_value)
+
+    print values
+
+#adds an echo to the wav file through a delay and lowering of volume
+def echo(sndFile, delay):
+    noise_out = wave.open('noise2.wav', 'w')
+    values = []
+    Channels = 1
+    s1 = sndFile
+    s2 = sndFile[:]
+    for index in range(delay, len(s1)):
+        echo = 0.6*s2[index-delay]
+        combo = (s1, index) + echo
+        s1[index] = combo
+        packaged_value = struct.pack("<h", s1[index])
+        for j in xrange(Channels):
+            values.append(packaged_value)
+    value_str = ''.join(values)
+    noise_out.writeframes(value_str)
+    noise_out.close()
+    return values
+
+#increases volume by making the amplitude of the waves bigger
+def increase_volume(frames, length):
+    for i in xrange(length):
+        frames[i] *= 2
+
+#Creates blank file and adds whitenoise
+def whitenoise():
+    length = 44100 * 0.5 #seconds
+    amplitude = 32767
+
+
+    #creates blank noise
+    noise = wave.open('whitenoise.wav', 'w')
+    noise.setparams((1, 2, 44100, length, 'NONE', 'Not compressed'))
+
+    #Generates white noise
+    for pos in range(int(length)):
+        rawsample = random.uniform(-1, 1)
+        samplevalue = int(amplitude * rawsample)
+        packedvalue = struct.pack("<h", samplevalue)
+        noise.writeframes(packedvalue)
+        print '' + str(pos) + ' of ' + str(length) + ': ' + packedvalue
+    return noise
+
+whitenoise()
